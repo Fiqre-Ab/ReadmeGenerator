0fcc45084eace8bb58ddd0c3d7edf9438f890178
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-10-19 22:57:59 -0500

    adding README

--- README.md
+++ README.md
@@ -1,35 +1,66 @@
-# hero
+# README Generator
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
 ## Description
-saving peaple
+
+The README Generator is a command-line application that allows users to quickly and easily create a professional README for a new project. It automates the process of generating a README by prompting the user for project information and then formatting that information into a well-structured README file.
 
 ## Table of Contents
+
 - [Installation](#installation)
 - [Usage](#usage)
+- [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [License](#license)
 - [Questions](#questions)
 
 ## Installation
-yes
+
+To install the README Generator, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Navigate to the project's root directory in the command line.
+3. Run the following command:
+
+
+This will install the required dependencies.
 
 ## Usage
-yes 
+
+To use the README Generator, run the following command in the project's root directory:
+
+
+You will be prompted to provide information about your project, and the application will generate a README based on your input.
 
 ## License
-![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
 This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
-  // Include the license section generated using renderLicenseSection
 
 ## Contributing
-just copy
+
+We welcome contributions from the open-source community. If you have suggestions, bug reports, or want to contribute to the development of this project, please follow these guidelines:
+
+1. Fork the project on GitHub.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push your changes to your fork.
+5. Submit a pull request.
 
 ## Tests
-git pull origin main and copy mt colone
+
+To run tests for this project, follow these steps:
+
+1. Ensure that the project dependencies are installed (see Installation section).
+2. In the command line, run the following command:
+
+
+This will execute the tests and provide feedback on the project's functionality.
 
 ## Questions
+
 If you have any questions or need further assistance, please feel free to reach out to me:
 
-GitHub: [@github](https://github.com/@github)
-Email: harnet
+GitHub: [YourGitHubUsername](https://github.com/Fiqre-Ab/ReadmeGenerator)
+
+Email: [youremail@example.com]
--- index.js
+++ index.js
@@ -5,56 +5,65 @@ const  generateMarkdown=require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 //add questions:added readme sections (descriptions,usage,license,etc)
 const questions = [
- {
-    type: "input",
-    message: "What is your project title?",
-    name: "title",
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: (input) => input.trim() !== '' || 'Please provide a title for your project.',
   },
   {
-    type: "input",
-    message: "Please provide a description of your project:",
-    name: "description",
+    type: 'input',
+    name: 'description',
+    message: 'Please describe your project:',
+    validate: (input) => input.trim() !== '' || 'Please provide a project description.',
   },
   {
-    type: "input",
-    message: "How can someone install your project?",
-    name: "installation",
+    type: 'input',
+    name: 'installation',
+    message: 'How can someone install your project?',
+    validate: (input) => input.trim() !== '' || 'Please provide installation instructions.',
   },
   {
-    type: "input",
-    message: "How can someone use your project?",
-    name: "usage",
+    type: 'input',
+    name: 'usage',
+    message: 'How can someone use your project?',
+    validate: (input) => input.trim() !== '' || 'Please provide usage information.',
   },
   {
-    type: "list",
-    message: "Choose a license for your project:",
-    name: "license",
-    choices: ["MIT", "GNU GPLv3", "Apache 2.0", "Other", "None"],
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'Other', 'None'],
   },
   {
-    type: "input",
-    message: "What are your contribution guidelines?",
-    name: "contributing",
+    type: 'input',
+    name: 'contributing',
+    message: 'Explain your contribution guidelines (if any):',
   },
   {
-    type: "input",
-    message: "How should someone run tests on your project?",
-    name: "tests",
+    type: 'input',
+    name: 'tests',
+    message: 'How should someone run tests on your project (if applicable):',
   },
   {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "github",
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+    validate: (input) => input.trim() !== '' || 'Please provide your GitHub username.',
   },
   {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address (for questions and inquiries):',
+    validate: (input) =>
+      /\S+@\S+\.\S+/.test(input) || 'Please provide a valid email address.',
   },
 ];
 
 
+
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName,data,(err)=>{
         err?console.error("error writing to file",err):console.log("README generated successfully!")
@@ -64,6 +73,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
 
+//call generateMarkdown function on the data collected
     inquirer.prompt(questions).then((response)=>{const markdown= generateMarkdown(response)
     
     writeToFile("README.md", markdown);
@@ -72,6 +82,3 @@ function init() {
 
 // Function call to initialize app
 init();
-//use inquiter to dipsplay questions to user
-//call generateMarkdown function on the data collected
-//take what is returned to us,then pass it to the writeFile function 
